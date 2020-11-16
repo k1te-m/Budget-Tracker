ebf3d9c43448cb4075700dd36978d31bb0efbdb8
k1te <kmiller343@gmail.com>
2020-11-16 12:39:07 -0600

    created webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  mode: "development",
+
+  // add entry points for JavaScript files for the three pages, home, detail, and favorites.
+  entry: {
+    // home: "./public/assets/js/home.js",
+    // detail: "./public/assets/js/detail.js",
+    // favorites: "./public/assets/js/favorites.js"
+  },
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "[name].bundle.js"
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: false,
+      name: "Budget Tracker",
+      short_name: "Budget Tracker",
+      description: "An application for tracking budgets",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      start_url: "/",
+    //   icons: [{
+    //     src: path.resolve("public/assets/images/icons/icon-192x192.png"),
+    //     sizes: [96, 128, 192, 256, 384, 512],
+    //     destination: path.join("assets", "icons")
+    //   }]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
+module.exports = config;
