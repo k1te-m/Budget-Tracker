f52bab5fc0b14311ad145c90f3967f729bd0310a
k1te <kmiller343@gmail.com>
2020-11-16 21:14:39 -0600

    updated files to cache for new folder structure

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,12 @@
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/assets/css/styles.css",
-  "/assets/js/index.js",
-  "/assets/js/indexedDb.js",
-  "/assets/images/icons/icon-192x192.png",
-  "/assets/images/icons/icon-512x512.png",
+  "/styles.css",
+  "/index.js",
+  "/indexedDb.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/manifest.json"
 ];
 
 const CACHE_NAME = "static-cache-v1";
