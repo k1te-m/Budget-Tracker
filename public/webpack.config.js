01861662cc818cc035695e00ac25269e53198c95
k1te <kmiller343@gmail.com>
2020-11-16 20:39:11 -0600

    working on webpack

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,48 +1,49 @@
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
 
 const config = {
+  mode: "development",
+
   entry: {
-    app: '/public/assets/js/index.js',
+    index: "./index.js",
+    indexedDb: "./indexedDb.js",
   },
   output: {
-    path: __dirname + '/public/dist',
-    filename: '[name].bundle.js',
+    path: __dirname + "/dist",
+    filename: "[name].bundle.js",
   },
-  mode: 'development',
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: false,
+      name: "Budget Tracker",
+      short_name: "Budget",
+      description: "Application that allows you to track your budget.",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      start_url: "/",
+      icons: [
+        {
+          src: path.resolve("icons/icon-192x192.png"),
+          sizes: [192, 512],
+          destination: path.join("", "icons"),
+        },
+      ],
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ["@babel/preset-env"],
           },
         },
       },
     ],
   },
-  plugins: [
-    new WebpackPwaManifest({
-      fingerprints: false,
-      name: 'Budget Tracker',
-      short_name: 'Budget Tracker',
-      description: 'An application that allows you to track your budget.',
-      background_color: '#01579b',
-      theme_color: '#ffffff',
-      'theme-color': '#ffffff',
-      start_url: '/',
-      icons: [
-        {
-          src: path.resolve('public/assets/images/icons/icon-192x192.png'),
-          sizes: [192, 512],
-          destination: path.join('assets', 'images', 'icons'),
-        },
-      ],
-    }),
-  ],
 };
-
 module.exports = config;
